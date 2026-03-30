76991a444874c2c87dc9e74902d4c3c920e4743c
prnsh <58560660+pranshuj73@users.noreply.github.com>
2026-03-31 01:47:25 +0530

    init

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Blurb';
+export const SITE_DESCRIPTION = 'Minimalist digital cards for instant, in-person networking.';
